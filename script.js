30a82511bc720aa16f1b96a02a9478b7d4cd8d11
SharadhiPrabhu <146694704+SharadhiPrabhu@users.noreply.github.com>
2024-07-16 21:45:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-const apiKey = 'e8855d3e8c674e328ce55414240707';  // Replace 'YOUR_API_KEY' with your actual API key
-const form = document.getElementById('locationForm');
-const weatherInfo = document.getElementById('weatherInfo');
+const apiKey = "e8855d3e8c674e328ce55414240707";
+const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
+const searchBox = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search-btn");
+const weatherIcon = document.querySelector(".weather-icon");
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const locationInput = document.getElementById('locationInput').value;
-    getWeather(locationInput);
-});
+async function getWeather(city) {
+    const response = await fetch(`${apiUrl}${city}`);
 
-async function getWeather(location) {
-    try {
-        const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json();
+        console.log(data);
+        document.querySelector(".city").innerHTML = data.location.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.current.temp_c) + "°c";
+        document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.current.wind_kph + " km/hr";
+
+        switch (data.current.condition.text) {
+            case "Clouds":
+                weatherIcon.src = "cloud.jpg";
+                break;
+            case "Clear":
+                weatherIcon.src = "clear.jpg";
+                break;
+            case "Rain":
+                weatherIcon.src = "rain.jpg";
+                break;
+            case "Drizzle":
+                weatherIcon.src = "drizzle.jpg";
+                break;
+            case "Mist":
+                weatherIcon.src = "mist.jpg";
+                break;
+            
         }
-        const weatherData = await response.json();
-        
-        const { location: { name }, current: { temp_c, humidity, condition: { text } } } = weatherData;
-        
-        weatherInfo.innerHTML = `
-            <h2>Current Weather in ${name}</h2>
-            <p><strong>Temperature:</strong> ${temp_c}°C</p>
-            <p><strong>Humidity:</strong> ${humidity}%</p>
-            <p><strong>Description:</strong> ${text}</p>
-        `;
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
-        weatherInfo.innerHTML = '<p>Failed to fetch weather data. Please try again later.</p>';
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
 }
+
+searchBtn.addEventListener("click", () => {
+    getWeather(searchBox.value);
+});
